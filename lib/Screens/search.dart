7397478f0126986b6d93530f2b7679e77c4f3939
Oğuz Ünlüerler <oguzunluerler1@gmail.com>
2022-06-03 17:07:20 +0300

    ikinci commit

--- lib/Screens/search.dart
+++ lib/Screens/search.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:togla_pratik/Screens/posts_detailed.dart';
 
@@ -12,7 +14,16 @@ class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: TextField(
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.search),
+            border: OutlineInputBorder(),
+            contentPadding: EdgeInsets.zero
+          ),
+        ),
+      ),
       body: bodyMethod(),
       
     );
@@ -39,8 +50,21 @@ class _SearchState extends State<Search> {
               )
             );
           },
+          onLongPress: (){
+            showDialog(
+              context: context, 
+              builder: (BuildContext) => AlertDialog(
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.network(rresim,fit: BoxFit.cover,)
+                  ],
+                ),
+              ) 
+            );
+          },
           child: Image.network(rresim,fit: BoxFit.cover,)
-        );        
+        );
       }
     );
   }
